c30462770df67302299aec3d721a6a6bf01060b7
dazedavid <36707342+dazedavid@users.noreply.github.com>
2020-04-28 00:10:49 -0500

    Removed braces

--- Azure/main.tf
+++ Azure/main.tf
@@ -7,37 +7,37 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   name     = "var.resource_group_name"
-  location = "${var.location}"
+  location = "var.location"
 }
 
 
 ### Network
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.virtual_network_name}"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  name                = "var.virtual_network_name"
+  location            = "azurerm_resource_group.rg.location"
+  resource_group_name = "azurerm_resource_group.rg.name"
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "${var.subnet_name}"
-  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  name                 = "var.subnet_name"
+  resource_group_name  = "azurerm_resource_group.rg.name"
   address_prefix       = "10.0.0.0/24"
-  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  virtual_network_name = "azurerm_virtual_network.vnet.name"
 }
 
 resource "azurerm_public_ip" "pip" {
-  name                         = "${var.public_ip_name}"
-  location                     = "${azurerm_resource_group.rg.location}"
-  resource_group_name          = "${azurerm_resource_group.rg.name}"
+  name                         = "var.public_ip_name"
+  location                     = "azurerm_resource_group.rg.location"
+  resource_group_name          = "azurerm_resource_group.rg.name"
   allocation_method = "Dynamic"
   }
 
 resource "azurerm_network_security_group" "allows" {
-  name = "${var.network_security_group_name}"
-  location = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  name = "var.network_security_group_name"
+  location = "azurerm_resource_group.rg.location"
+  resource_group_name = "azurerm_resource_group.rg.name"
 
   security_rule {
     name                       = "AllowSshInBound"
@@ -64,16 +64,16 @@ resource "azurerm_network_security_group" "allows" {
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                      = "${var.network_interface_name}"
-  location                  = "${azurerm_resource_group.rg.location}"
-  resource_group_name       = "${azurerm_resource_group.rg.name}"
+  name                      = "var.network_interface_name"
+  location                  = "azurerm_resource_group.rg.location"
+  resource_group_name       = "azurerm_resource_group.rg.name"
   
 
   ip_configuration {
-    name                          = "${var.network_interface_name}-configuration"
-    subnet_id                     = "${azurerm_subnet.subnet.id}"
+    name                          = "var.network_interface_name-configuration"
+    subnet_id                     = "azurerm_subnet.subnet.id"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = "${azurerm_public_ip.pip.id}"
+    public_ip_address_id          = "azurerm_public_ip.pip.id"
   }
 }
 
@@ -81,11 +81,11 @@ resource "azurerm_network_interface" "nic" {
 ### Virtual Machine
 
 resource "azurerm_virtual_machine" "vm" {
-  name                  = "${var.virtual_machine_name}"
-  location              = "${azurerm_resource_group.rg.location}"
-  resource_group_name   = "${azurerm_resource_group.rg.name}"
-  network_interface_ids = ["${azurerm_network_interface.nic.id}"]
-  vm_size               = "${var.virtual_machine_size}"
+  name                  = "var.virtual_machine_name"
+  location              = "azurerm_resource_group.rg.location"
+  resource_group_name   = "azurerm_resource_group.rg.name"
+  network_interface_ids = ["azurerm_network_interface.nic.id"]
+  vm_size               = "var.virtual_machine_size"
 
   storage_image_reference {
     publisher = "Canonical"
@@ -95,29 +95,29 @@ resource "azurerm_virtual_machine" "vm" {
   }
 
   storage_os_disk {
-    name              = "${var.virtual_machine_osdisk_name}"
+    name              = "var.virtual_machine_osdisk_name"
     create_option     = "FromImage"
-    managed_disk_type = "${var.virtual_machine_osdisk_type}"
+    managed_disk_type = "var.virtual_machine_osdisk_type"
   }
 
   os_profile {
-    computer_name  = "${var.virtual_machine_computer_name}"
-    admin_username = "${var.admin_username}"
+    computer_name  = "var.virtual_machine_computer_name"
+    admin_username = "var.admin_username"
   }
 
   os_profile_linux_config {
     disable_password_authentication = true
     ssh_keys {
       path     = "/home/${var.admin_username}/.ssh/authorized_keys"
-      key_data = "${var.ssh_public_key_data}"
+      key_data = "var.ssh_public_key_data"
     }
   }
 
   connection {
     type        = "ssh"
-    host        = "${azurerm_public_ip.pip.ip_address}"
-    user        = "${var.admin_username}"
-    private_key = "${var.ssh_private_key_data}"
+    host        = "azurerm_public_ip.pip.ip_address"
+    user        = "var.admin_username"
+    private_key = "var.ssh_private_key_data"
   }
 
   provisioner "remote-exec" {
