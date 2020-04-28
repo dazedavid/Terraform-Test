bbd93571529906ad1ff3b0237c9be2c81894a670
dazedavid <36707342+dazedavid@users.noreply.github.com>
2020-04-28 08:43:34 -0500

    colon removed

--- Azure/main.tf
+++ Azure/main.tf
@@ -65,8 +65,8 @@ resource "azurerm_network_security_group" "allows" {
 
 resource "azurerm_network_interface" "nic" {
   name                      = "var.network_interface_name"
-  location                  = "azurerm_resource_group.rg.location"
-  resource_group_name       = "azurerm_resource_group.rg.name"
+  location                  = azurerm_resource_group.rg.location
+  resource_group_name       = azurerm_resource_group.rg.name
   
 
   ip_configuration {
