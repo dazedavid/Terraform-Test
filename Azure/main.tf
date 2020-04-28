9a41917b5141c2d46f3e537a1988c8f447e8e622
dazedavid <36707342+dazedavid@users.noreply.github.com>
2020-04-27 23:18:38 -0500

    Update main.tf

--- Azure/main.tf
+++ Azure/main.tf
@@ -1,9 +1,6 @@
 
 ### General
-provider "azurerm" {
-    version = "~>2.3.0"
-  features {}
-  }
+
 resource "azurerm_resource_group" "rg" {
   name     = "${var.resource_group_name}"
   location = "${var.location}"
